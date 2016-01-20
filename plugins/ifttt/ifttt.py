33357bce2b91af265a4b168c22d6493c95a0de61
bverschueren <verschueren.bram@gmail.com>
2016-01-20 13:50:12 +0000

    receive message and match content with config

--- plugins/ifttt/ifttt.py
+++ plugins/ifttt/ifttt.py
@@ -1,32 +1,49 @@
+#!/usr/bin/env python
+
 import time
 import yaml
 import os.path
+import requests
+import copy
 
 class ifttt(object):
 
 	def __init__(self, config=None):
-
-       	        print "plugin loaded"
                 if config is not None and os.path.isfile(config):
                     self.config=yaml.load(file(config))
-		self.SLACK_TOKEN='ABC123'
+		    self.SLACK_TOKEN=self.config['SLACK_TOKEN']
 
-        crontable = []
-        outputs = []
+        crontable = [] # might be checked by calling class
+        outputs = [] # might be checked by calling class
 
         def process_message(self, data):
 		search_list=[data[ent] for ent in ['team', 'channel', 'user', 'text']]
-		print self.getFirstAction(self.config, search_list).format('ABC123', data['user'])
-                if data['channel'] == self.config['channel'] and data['user'] == self.config['user']:
-			print "[{}] {}: {}".format(data['channel'], data['user'], data['text'])
+		action_dict=self.getFirstAction(self.config['team'], search_list)
+		if action_dict is not None:
+			action = action_dict['url'] + action_dict['chain'].pop(0) + '?' 
+			for param in action_dict['extra_params']:
+				action += param
+			post_data=data.copy()
+			post_data.update(action_dict)
+			post_data.update({"reply_with": self.SLACK_TOKEN, "email": self.getUserEmailFromId(data['user']), "username": self.getUserNameFromId(data['user'])})
+			print requests.post(action, params=post_data).text
 
 	def getFirstAction(self, config_dict, search_list):
         	searching=search_list.pop(0)
-        	for keydict in (config_dict[config_dict.keys()[0]]):
-                	if isinstance(keydict, dict):
-                        	for value in keydict.keys():
-                                	if searching == value:
-                                        	if keydict[value].has_key('action'):
-	                                                return keydict[value]['action']
+        	for subdict in (config_dict):
+                	if isinstance(subdict, dict):
+                        	for key in subdict.keys():
+                                	if searching == key:
+                                        	if subdict[key].has_key('action'):
+	                                                return copy.deepcopy(subdict[key]['action'])
         	                                else:
-							return self.getFirstAction(keydict[value], search_list)
+							return self.getFirstAction(subdict[key][subdict[key].keys()[0]], search_list)
+			else:
+				return None	
+
+	def getUserEmailFromId(self, user_id):
+		return requests.get('https://slack.com/api/users.info', params={'token': self.SLACK_TOKEN, 'user': user_id}).json()['user']['profile']['email']
+
+	def getUserNameFromId(self, user_id):
+                return requests.get('https://slack.com/api/users.info', params={'token': self.SLACK_TOKEN, 'user': user_id}).json()['user']['name']
+
